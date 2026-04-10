fb4d8f923d983fcb595af109a3c141c5c0be5b5b
buzincuemanuel <emanuel.buzincu@stud.ubbcluj.ro>
2026-04-10 20:49:55 +0300

    feat: implement complete JWT authentication and security filter

--- backend/src/main/java/io/github/b4official/mail/repository/InMemoryUserRepository.java
+++ backend/src/main/java/io/github/b4official/mail/repository/InMemoryUserRepository.java
@@ -1,6 +1,8 @@
 package io.github.b4official.mail.repository;
 
 import io.github.b4official.mail.domain.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,10 +12,12 @@ import java.util.Optional;
 @Repository
 public class InMemoryUserRepository {
 
+    private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
+
     private final List<User> users = new ArrayList<>();
 
     public InMemoryUserRepository() {
-        users.add(new User(1L, "emibz", "emi", "buzincu", "emi@test", "temporary"));
+        users.add(new User(1L, "emibz", "emi", "buzincu", "emi@test", encoder.encode("test") ));
     }
 
     public Optional<User> findByUsername(String username) {
